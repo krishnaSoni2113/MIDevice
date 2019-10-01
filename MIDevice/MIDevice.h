15a74d27575b18a984fe61187c606b7849aa9c95
KRISHNA SONI <mac-0005@example.com>
2019-10-01 15:31:39 +0530

    - Initial commit

---
+++ MIDevice/MIDevice.h
@@ -0,0 +1,19 @@
+//
+//  MIDevice.h
+//  MIDevice
+//
+//  Created by mac-0005 on 01/10/19.
+//  Copyright © 2019 mac-0005. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MIDevice.
+FOUNDATION_EXPORT double MIDeviceVersionNumber;
+
+//! Project version string for MIDevice.
+FOUNDATION_EXPORT const unsigned char MIDeviceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MIDevice/PublicHeader.h>
+
+
